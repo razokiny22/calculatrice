59907a5370f041bc4d6ee91e33343a6379aae55f
mathieu <mathieutoky22@gmail.com>
2022-04-05 13:26:07 +0300

    first push

---
+++ app.js
@@ -0,0 +1,45 @@
+const display = document.querySelector('#display');
+const displayResult = document.querySelector('#displayResult');
+const btn = document.querySelectorAll('.boutton');
+
+btn.forEach((item) =>{
+    item.onclick = () => {
+        if(item.id === 'clear'){
+            display.innerText = '';
+            displayResult.innerText = '';
+        }else if(item.id === 'backSpace'){
+            let str = display.innerText;
+            display.innerText = str.substr(0,str.length-1);
+            if(eval(display.innerText)==undefined){
+                displayResult.innerText = '';
+            }else{
+                displayResult.innerText = eval(display.innerText);
+            }
+           
+            }else if(display.innerText != '' && item.id =='equal'){
+                if(eval(display.innerText)=='Infinity' || eval(display.innerText)=='NaN' || eval(display.innerText)=='-Infinity'){
+                    display.innerText = '';
+                    displayResult.innerText ='Math Error';
+                    setTimeout(()=> (displayResult.innerText =''), 2000);
+                }
+                else{
+                    displayResult.innerText = eval(display.innerText);
+                    display.innerText = '';
+                }
+                }else if(display.innerText == '' && item.id == 'equal'){
+                        displayResult.innerText = 'Empty!';
+                        setTimeout(()=> (displayResult.innerText =''), 2000);
+                    }else {
+                        display.innerText += item.id;
+                        if(eval(display.innerText)=='Infinity' || eval(display.innerText)=='NaN' || eval(display.innerText)=='-Infinity'){
+                            displayResult.innerText = '';
+                        }else{
+                            displayResult.innerText = eval(display.innerText)
+                        }
+                       
+                      
+                        
+                    }
+        }
+                
+})
