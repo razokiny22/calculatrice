ce5d172b6a4f2db930da68f5a5d52f80de48f1a0
mathieu <mathieutoky22@gmail.com>
2022-04-05 16:54:14 +0300

    linear gradient

--- app.js
+++ app.js
@@ -19,8 +19,10 @@ btn.forEach((item) =>{
             }else if(display.innerText != '' && item.id =='equal'){
                 if(eval(display.innerText)=='Infinity' || eval(display.innerText)=='NaN' || eval(display.innerText)=='-Infinity'){
                     display.innerText = '';
+                    displayResult.style.color = 'red';
                     displayResult.innerText ='Math Error';
                     setTimeout(()=> (displayResult.innerText =''), 2000);
+                    displayResult.style.color = 'white';
                 }
                 else{
                     displayResult.innerText = eval(display.innerText);
